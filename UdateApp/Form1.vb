0792a4e031b79a00a77a167930b109b8b5838288
Cavit Dıraman <cavit.draman@gmail.com>
2024-05-27 08:15:16 +0300

    Proje dosyası ekle.

---
+++ UdateApp/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+Public Class Form1
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim currentVersion As String = "1.0.0" ' Mevcut program sürümü
+        Dim releasesUrl As String = "https://raw.githubusercontent.com/kullaniciadi/repoadi/main/releases.json" ' GitHub'daki releases.json dosyasının URL'si
+
+        If Updater.CheckForUpdate(currentVersion, releasesUrl) Then
+            ' Güncelleme yapıldıysa, program burada yeniden başlatılacak
+            Application.Exit()
+        End If
+
+    End Sub
+
+End Class
---
+++ UdateApp/Updater.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+
+Public Class Updater
+
+    Public Shared Function CheckForUpdate(currentVersion As String, releasesUrl As String) As Boolean
+        Try
+            Dim client As New WebClient()
+            Dim jsonData As String = client.DownloadString(releasesUrl)
+            Dim releaseInfo As JObject = JObject.Parse(jsonData)
+            Dim latestVersion As String = releaseInfo("version").ToString()
+            Dim downloadUrl As String = releaseInfo("url").ToString()
+
+            If String.Compare(currentVersion, latestVersion) < 0 Then
+                DownloadUpdate(downloadUrl)
+                Return True
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Güncelleme kontrolü başarısız: " & ex.Message)
+        End Try
+
+        Return False
+    End Function
+
+    Private Shared Sub DownloadUpdate(url As String)
+        Try
+            Dim client As New WebClient()
+            Dim tempFile As String = Path.Combine(Path.GetTempPath(), "program_update.exe")
+            client.DownloadFile(url, tempFile)
+
+            ' Geçici dosyayı yeni sürüm olarak kopyalayın
+            Dim currentPath As String = Application.ExecutablePath
+            File.Copy(tempFile, currentPath, True)
+
+            ' Geçici dosyayı silin
+            File.Delete(tempFile)
+            MessageBox.Show("Güncelleme başarıyla indirildi. Program yeniden başlatılıyor...")
+            Application.Restart()
+        Catch ex As Exception
+            MessageBox.Show("Güncelleme indirilemedi: " & ex.Message)
+        End Try
+    End Sub
+End Class
