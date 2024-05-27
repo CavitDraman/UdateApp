d516e4639f324b619866bad7a62e0dd9bd3469bc
Cavit Dıraman <cavit.draman@gmail.com>
2024-05-27 08:34:27 +0300

    a

--- UdateApp/Form1.vb
+++ UdateApp/Form1.vb
@@ -6,7 +6,7 @@ Public Class Form1
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim currentVersion As String = "1.0.0" ' Mevcut program sürümü
-        Dim releasesUrl As String = "https://raw.githubusercontent.com/kullaniciadi/repoadi/main/releases.json" ' GitHub'daki releases.json dosyasının URL'si
+        Dim releasesUrl As String = "https://raw.githubusercontent.com/CavitDraman/UdateApp/master/UdateApp/releases.json"   ' GitHub'daki releases.json dosyasının URL'si
 
         If Updater.CheckForUpdate(currentVersion, releasesUrl) Then
             ' Güncelleme yapıldıysa, program burada yeniden başlatılacak
